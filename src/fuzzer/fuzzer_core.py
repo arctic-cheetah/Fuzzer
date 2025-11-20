faf326281fb70d3e5cf64522cec01b1dd32c9c7c
jj roberts-white <computerfido@gmail.com>
2025-11-21 04:40:23 +1100

    slow harness

--- src/fuzzer/fuzzer_core.py
+++ src/fuzzer/fuzzer_core.py
@@ -2,7 +2,10 @@ import os, re, random, subprocess
 import asyncio
 from typing import List, Callable
 from globals import mount_point
+import sys
 import zmq
+import zmq.asyncio
+import json
 
 NUM_TO_RUN = 50_000
 TIMEOUT = 1
@@ -25,41 +28,102 @@ class Fuzzer:
         self.binary_path = binary_path
         self.binary_name = re.search(r"(\w+)$", self.binary_path)[0]
         self.mutators = []
+        
+        self.ctx = zmq.asyncio.Context()
 
     def run_binary(self):
         with open(self.path_to_input, "rb") as f:
             seed = f.read()
 
-        events = zmq.socket(zmq.SUB)
+        return asyncio.run(self._run_binary(seed))
+
+    async def _run_binary(self, seed: bytes):
+        events = self.ctx.socket(zmq.SUB)
         events.connect("ipc:///tmp/fuzzer-events")
         events.setsockopt_string(zmq.SUBSCRIBE, "")
 
-        cmds = zmq.socket(zmq.REQ)
+        cmds = self.ctx.socket(zmq.REQ)
         cmds.connect("ipc:///tmp/fuzzer")
-
-        pipe = open("/tmp/fuzzer-pipe1", "wb")
 
         for x in range(NUM_TO_RUN):
             try:
+                # clear the line and print progress
+                print(f"\rFuzzing {self.binary_name}: {x}/{NUM_TO_RUN}", end="", flush=True)
+
                 if hasattr(self, "make_payload"):
                     data = self.make_payload(seed)
                 else:
                     data = self.mutate(seed)
 
-                # Notify harness to process input
-                cmds.send(f"exec;{self.binary_path}".encode())
-                pipe_name = cmds.recv()  # Wait for acknowledgment
-                print(f"sending input {x} to harness via {pipe_name.decode()}")
+                # Make a pipe to send data to harness
+                pipe_name = f"/tmp/fuzzer-pipe-{os.getpid()}-{x}"
+                os.mkfifo(pipe_name, 0o666)
                 
                 # Send data to harness via pipe
-                size_bytes = len(data).to_bytes(4, byteorder="little")
-                pipe.write(size_bytes)
-                pipe.write(data)
-                pipe.flush()
+                def write_pipe():
+                    try:
+                        with open(pipe_name, "wb") as p:
+                            p.write(data)
+                            p.flush()
+                    except BrokenPipeError:
+                        pass
+                    except Exception as e:
+                        print(f"Error writing to pipe: {e}")
 
-                print(events.recv().decode())  # Wait for harness to signal completion
-            except:
+                async def pipe_coroutine():
+                    try:
+                        await asyncio.to_thread(write_pipe)
+                    except asyncio.CancelledError:
+                        pass
+
+                c1 = asyncio.create_task(pipe_coroutine())
+
+                # Notify harness to process input
+                await cmds.send(f"exec;{self.binary_path};{pipe_name}".encode())
+                pid = await cmds.recv()  # Wait for acknowledgment
+                pid = int(pid.decode())
+
+                if pid == 0:
+                    if c1 is not None:
+                        c1.cancel()
+                    print(f"Failed to execute binary for iteration {x}")
+                    continue
+
+                async def read_event():
+                    msg = await events.recv()
+
+                    if c1 is not None:
+                        c1.cancel()
+
+                    return msg
+
+                c2 = asyncio.create_task(read_event())
+
+                await asyncio.gather(c1, c2)
+
+                msg = c2.result()
+                msg_str = msg.decode()
+
+                try:
+                    success_pid = int(msg_str)
+                except Exception:
+                    run_info = json.loads(msg_str)
+
+                    if run_info.get("status") == "signal":
+                        signum = run_info.get("signal")
+
+                        # ignore aborts
+                        if signum == 6:
+                            continue
+                        print("________________________________")
+                        print(f"Crashed at the {x} input")
+                        print(run_info)
+
+                        self.log_crash(run_info)
+
+            except Exception as x:
                 print(f"Error during fuzzing iteration {x}")
+                sys.exit(0)
                 continue
 
         '''for x in range(NUM_TO_RUN):
