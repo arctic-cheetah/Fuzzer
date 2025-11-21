8d375d33efb64eae094a3153ee070709e596c745
m0awie <115065404+m0awie@users.noreply.github.com>
2025-11-21 15:52:55 +1100

    Update main.py properly this time

--- src/fuzzer/main.py
+++ src/fuzzer/main.py
@@ -50,7 +50,7 @@ def main():
     input_arr = []
     for filename in os.listdir(binary_path):
         bin_arr.append(os.path.join(directory_path, filename))
-        example_inputs.append(os.path.join(example_inputs, filename))
+        input_arr.append(os.path.join(example_inputs, filename))
     
     test_arr = [input_arr, bin_arr]
 
